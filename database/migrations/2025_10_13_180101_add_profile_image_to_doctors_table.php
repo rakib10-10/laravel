6bf9438bb3233e930cf55a8198ba3ca99e401d78
rakib <mdtara0199@gmail.com>
2025-10-14 00:11:45 +0600

    Profile picture added

--- database/migrations/2025_10_13_180101_add_profile_image_to_doctors_table.php
+++ database/migrations/2025_10_13_180101_add_profile_image_to_doctors_table.php
@@ -6,23 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('doctors', function (Blueprint $table) {
-            //
+            if (!Schema::hasColumn('doctors', 'profile_image')) {
+                $table->string('profile_image')->nullable()->after('work_experience');
+            }
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('doctors', function (Blueprint $table) {
-            //
+            if (Schema::hasColumn('doctors', 'profile_image')) {
+                $table->dropColumn('profile_image');
+            }
         });
     }
 };
